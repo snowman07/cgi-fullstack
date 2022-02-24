b286d4c77c93228ae34ee97c1358b6c55b62d028
Arr Belrey Domingo <59627816+snowman07@users.noreply.github.com>
2022-02-24 11:39:31 -0700

    Dynamic fetch issue solved
    I can now search weather dynamically based on cities

--- week6-angular/exercise-angular-weather-app/src/app/Components/weather-main/weather-main.component.ts
+++ week6-angular/exercise-angular-weather-app/src/app/Components/weather-main/weather-main.component.ts
@@ -13,15 +13,19 @@ export class WeatherMainComponent implements OnInit {
 
   //TO DISPLAY THE CONTENT IN A WEBPAGE!!!!!
 
-  public cityname:string = '';
+  // public cityname:string = '';
 
   public cities: any; //any is a collection of objects
 
   GetWeatherData(cityname:string) {
-    this.weatherData.GetWeatherDataFromCity(this.cityname).subscribe(data => console.log(data));
-
+    this.weatherData.GetWeatherDataFromCity(cityname).subscribe(data => {
+      console.log(data);
+      this.cities = data;
+    }
+    );
+    
     //TO DISPLAY THE CONTENT IN A WEBPAGE!!!!!
-    this.weatherData.GetWeatherDataFromCity(this.cityname).subscribe(data => this.cities = data);
+    //this.weatherData.GetWeatherDataFromCity(this.cityname).subscribe(data => this.cities = data);
   }
 
   ngOnInit(): void {
